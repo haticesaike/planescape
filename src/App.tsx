a9183547e3378f7128bcc15160ed9a22188d1a07
Hatice Saike <haticesaike374@gmail.com>
2024-08-13 21:59:05 +0300

    components created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import FlightDetails from "./components/flightDetails/FlightDetails";
+import Header from "./components/header/Header";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <div className="header">
+          <Header/>
+      </div>
+
+      <div className="container">
+       <div className="flight-details">
+           <FlightDetails />
+       </div >
+        <div className="sidebar"><Sidebar /></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/flightDetails/FlightDetails.tsx
@@ -0,0 +1,5 @@
+import styles from "./FlightDetails.module.css";
+
+export default function FlightDetails() {
+  return <div>FlightDetails</div>;
+}
