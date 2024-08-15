e77b1dc5d85c0c5cf39ac4bb648778fa80e92df8
Hatice Saike <haticesaike374@gmail.com>
2024-08-15 14:03:45 +0300

    added sidebar component with initial styling

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,5 +1,30 @@
 import styles from "./Sidebar.module.css";
+import { FaCar } from "react-icons/fa";
+import { FaHotel } from "react-icons/fa";
+import { FaUmbrellaBeach } from "react-icons/fa";
+
+
+
 
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  return <div className={styles.sidebar}>
+    <div className={styles.container} style={{backgroundImage: "url('/src/assets/images/car.jpg')"}}>
+      <FaCar className={styles.icon}/>
+        <h3>CAR RENTALS</h3>
+
+    </div>
+    <div className={styles.container} style={{backgroundImage: "url('/src/assets/images/hotel.jpg')"}}>
+      <FaHotel className={styles.icon}/>
+      <h3>HOTELS</h3>
+
+
+    </div>
+    <div className={styles.container} style={{backgroundImage: "url('/src/assets/images/travel.jpg')"}}>
+      <FaUmbrellaBeach className={styles.icon}/>
+      <h3>TRAVEL PACKAGES</h3>
+
+
+    </div>
+
+  </div>;
 }
