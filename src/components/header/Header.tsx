128f4f6020e41ffb4d83629685f29555f30c5d0f
Hatice Saike <haticesaike374@gmail.com>
2024-08-15 14:02:07 +0300

    header style edited

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,35 @@
 import styles from "./Header.module.css";
+import { IoIosAirplane } from "react-icons/io";
+import { FaTag } from "react-icons/fa";
+import { IoEarth } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+
+
+
+
 
 function Header() {
-  return <div>Header</div>;
+  return <div className={styles.cover}>
+    <div className={styles.left}>
+      <div className={styles.logo}><IoIosAirplane className={styles.iconLogo} />
+      </div>
+      <div className={styles.search}>PLANE SCAPE</div>
+    </div>
+    <div className={styles.right}>
+      <div className={styles.item}>
+        <FaTag className={styles.icon}/>
+        Deals
+      </div>
+      <div className={styles.item}>
+        <IoEarth className={styles.icon}/>
+        Discover
+      </div>
+      <div className={styles.item}>
+        <CgProfile className={styles.icon}/>
+        Joane Smith
+      </div>
+    </div>
+  </div>;
 }
 
 export default Header;
